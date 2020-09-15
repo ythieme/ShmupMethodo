384d173e087f83dec6e388f7ac6f60892825053a
DESKTOP-44DCLRC\Yovann <y.thieme@rubika-edu.com>
2020-09-15 16:28:27 +0200

    Ajout tir

---
+++ ShmupMethodo/Assets/YT_Script/YT_BulletDestroy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YT_BulletDestroy : MonoBehaviour
+{
+    private float TimerDestroy;
+
+    [SerializeField]
+    float TimeToDestroy = 100f;
+
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+
+
+        if (col.gameObject.CompareTag("Wall"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Update()
+    {
+
+        TimerDestroy += Time.deltaTime;
+        if (TimerDestroy > TimeToDestroy)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
--- ShmupMethodo/Assets/YT_Script/YT_Bullets.cs
+++ ShmupMethodo/Assets/YT_Script/YT_Bullets.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class YT_Bullets : MonoBehaviour
 {
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+
+    public float bulletForce = 20f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,18 @@ public class YT_Bullets : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            shoot();
+        }
+
+    }
+
+    void shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.up* bulletForce, ForceMode2D.Impulse);
+
     }
 }
