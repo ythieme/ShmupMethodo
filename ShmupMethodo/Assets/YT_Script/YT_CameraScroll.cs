49ee6f057d78f909fced7c05a939c6158ce39ad6
DESKTOP-44DCLRC\Yovann <y.thieme@rubika-edu.com>
2020-09-15 16:10:09 +0200

    Ajout boundaries camera, ajout placeholder sprite, ajout prefab bullets

---
+++ ShmupMethodo/Assets/YT_Script/YT_CameraScroll.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YT_CameraScroll : MonoBehaviour
+{
+    [SerializeField]
+    private float curCameraSpeed = 0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.right * curCameraSpeed * Time.deltaTime;
+    }
+
+    /*
+    private void FixedUpdate()
+    {
+        transform.position += Vector3.right * curCameraSpeed * Time.fixedDeltaTime;
+    }*/
+
+    public void SetCameraSpeed(float newCameraSpeed)
+    {
+        curCameraSpeed = newCameraSpeed;
+    }
+
+    public float GetCameraSpeed()
+    {
+        return curCameraSpeed;
+    }
+
+}
--- ShmupMethodo/Assets/YT_Script/YT_ScreenBoundaries.cs
+++ ShmupMethodo/Assets/YT_Script/YT_ScreenBoundaries.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class YT_ScreenBoundaries : MonoBehaviour
 {
+    public Camera mainCamera;
     private Vector2 screenbounds;
+    private float objectWitdth;
+    private float objectHeight;
     // Start is called before the first frame update
     void Start()
     {
-      screenbounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+      screenbounds = mainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, mainCamera.transform.position.z));
+
     }
 
     // Update is called once per frame
@@ -16,7 +20,7 @@ public class YT_ScreenBoundaries : MonoBehaviour
     {
         Vector3 viewPos = transform.position;
         viewPos.x = Mathf.Clamp(viewPos.x, screenbounds.x, screenbounds.x * -1);
-        viewPos.x = Mathf.Clamp(viewPos.y, screenbounds.y, screenbounds.y * -1);
+        viewPos.y = Mathf.Clamp(viewPos.y, screenbounds.y, screenbounds.y * -1);
         transform.position = viewPos;
     }
 }
