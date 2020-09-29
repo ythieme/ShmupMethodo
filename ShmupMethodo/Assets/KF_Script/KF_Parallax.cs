cac5b3134e08e9a085557db46a18aacd80a929f5
Ezskel <k.faynot@rubika-edu.com>
2020-09-29 16:50:11 +0200

    Parallax

---
+++ ShmupMethodo/Assets/KF_Script/KF_Parallax.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KF_Parallax : MonoBehaviour
+{
+    [SerializeField] float multiplier_x = 0.0f;
+    [SerializeField] float multiplier_y = 0.0f;
+    [SerializeField] bool horizontalOnly = true;
+
+    private Transform cameraTransform;
+
+    public Vector3 startCameraPos;
+    public Vector3 startPos;
+
+    void Start()
+    {
+        cameraTransform = Camera.main.transform;
+        startCameraPos = cameraTransform.position;
+        startPos = transform.position;
+    }
+
+
+    private void LateUpdate()
+    {
+        var position = startPos;
+        if (horizontalOnly)
+            position.x += multiplier_x * (cameraTransform.position.x - startCameraPos.x);
+        else
+            position.x += multiplier_x * (cameraTransform.position.x - startCameraPos.x);
+        position.y += multiplier_y * (cameraTransform.position.y - startCameraPos.y);
+
+        transform.position = position;
+    }
+
+}
